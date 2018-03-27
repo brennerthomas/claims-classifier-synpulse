14f51dca1c44a2e9e01bdb7256659d0ad0950658
brennerthomas <tommy.brenner@gmx.net>
2018-03-27 17:05:38 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -14,12 +14,12 @@ import csv
 # ==================================================
 
 # Data Parameters
-tf.flags.DEFINE_string("positive_data_file", "./data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
-tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data.")
+tf.flags.DEFINE_string("positive_data_file", "./data/positive_examples.txt", "Data source for the positive data.")
+tf.flags.DEFINE_string("negative_data_file", "./data/negative_examples.txt", "Data source for the negative data.")
 
 # Eval Parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_string("checkpoint_dir", "", "Checkpoint directory from training run")
+tf.flags.DEFINE_string("checkpoint_dir", "runs/test/", "Checkpoint directory from training run")
 tf.flags.DEFINE_boolean("eval_train", False, "Evaluate on all training data")
 
 # Misc Parameters
