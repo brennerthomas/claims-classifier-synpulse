640b8cdb27ac3977f76fe8ce109c415cba184fb9
brennerthomas <tommy.brenner@gmx.net>
2018-03-27 17:06:09 +0200

    Update train.py

--- train.py
+++ train.py
@@ -107,7 +107,8 @@ with tf.Graph().as_default():
 
         # Output directory for models and summaries
         timestamp = str(int(time.time()))
-        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+        #out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", "test"))
         print("Writing to {}\n".format(out_dir))
 
         # Summaries for loss and accuracy
