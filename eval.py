c57d85668596ecc126d46f258fecf99a41fa8ccd
brennerthomas <tommy.brenner@gmx.net>
2018-03-27 17:25:54 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -19,13 +19,14 @@ tf.flags.DEFINE_string("negative_data_file", "./data/negative_examples.txt", "Da
 
 # Eval Parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_string("checkpoint_dir", "./runs/test/", "Checkpoint directory from training run")
+#tf.flags.DEFINE_string("checkpoint_dir", "./runs/test/", "Checkpoint directory from training run")
 tf.flags.DEFINE_boolean("eval_train", False, "Evaluate on all training data")
 
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
 
+checkpoint_dir = "./runs/test/"
 
 FLAGS = tf.flags.FLAGS
 
@@ -43,7 +44,7 @@ else:
     y_test = [1, 0]
 
 # Map data into vocabulary
-vocab_path = os.path.join(FLAGS.checkpoint_dir, "vocab")
+vocab_path = os.path.join("./runs/test/", "vocab")
 vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
 x_test = np.array(list(vocab_processor.transform(x_raw)))
 
@@ -51,7 +52,7 @@ print("\nEvaluating...\n")
 
 # Evaluation
 # ==================================================
-checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+checkpoint_file = tf.train.latest_checkpoint(checkpoint_dir)
 graph = tf.Graph()
 with graph.as_default():
     session_conf = tf.ConfigProto(
@@ -60,7 +61,7 @@ with graph.as_default():
     sess = tf.Session(config=session_conf)
     with sess.as_default():
         # Load the saved meta graph and restore variables
-        print("{}.meta".format(checkpoint_file))
+        saver = tf.train.import_meta_graph("{}checkpoints/{}.meta".format(checkpoint_dir, checkpoint_file))
         saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
         saver.restore(sess, checkpoint_file)
 
