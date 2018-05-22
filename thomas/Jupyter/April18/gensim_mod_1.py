c3cfde76c7df17e14e6c2d853c738291557073de
brennerthomas <ubuntu@ip-172-31-3-209.eu-west-1.compute.internal>
2018-05-22 14:57:59 +0000

    Next Step

---
+++ thomas/Jupyter/April18/gensim_mod_1.py
@@ -0,0 +1,8 @@
+import gensim
+import pandas as pd
+
+dataset = pd.read_csv("data/movies_metadata_mod.csv", sep=",")
+
+
+
+print("Hallo")
--- thomas/Jupyter/gensim_mod_1.py
+++
@@ -1,8 +0,0 @@
-import gensim
-import pandas as pd
-
-dataset = pd.read_csv("data/movies_metadata_mod.csv", sep=",")
-
-
-
-print("Hallo")
